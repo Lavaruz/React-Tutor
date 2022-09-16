7176d108d835643d322fec1b831cbc37968e10f3
Assami Muzaki <muzakiassami@gmail.com>
2022-09-16 21:50:06 +0700

    simple react hook (useState, useEffect)

--- 2/src/App.js
+++ 2/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
-import Introduction from './components/introduction'
-import {useState} from 'react'
+// import Introduction from './components/introduction'
+import React, {useState, useEffect} from 'react'
 
 function App() {
 
-  const [pacarSaya, setPacarSaya] = useState(1)
+  // USE STATE HOOK
+  const [pacarSaya, setPacarSaya] = useState(0)
+  const [namaPacar, setNamaPacar] = useState()
+
+  useEffect(() => {
+    if(pacarSaya > 1){
+      setNamaPacar('Rahasia')
+    }else if(pacarSaya > 0){
+      setNamaPacar('Kaela')
+    }else{
+      setNamaPacar()
+    }
+  }, [pacarSaya]);
 
   return (
     <div>
       <h1>Saya punya {pacarSaya} pacar</h1>
+      <h3>Nama Pacar : {namaPacar}</h3>
 
       <button onClick={() => setPacarSaya(prev => prev+1)}>Tambah Pacar</button>
       <button onClick={() => setPacarSaya(prev => pacarSaya <= 0 ? 0 : prev-1)}>Putus</button>
